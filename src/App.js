d3989c2b09fdbae32da813d6633c647aaa2cf1e4
SIBO-BALDE <baldesibo@gmail.com>
2024-01-02 22:22:11 +0000

    correction conflit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {Route, Routes} from 'react-router-dom'
 import PageAccueil from './Components/PageAccueil/PageAccueil';
 import Connexion from './Components/Authentification/Connexion';
 import DashbordUtilisateur from './Components/DashbordUtilisateur/DashbordUtilisateur';
-import Dashbord from './Components/DashbordAdmin/Dashbord';
+// import Dashbord from './Components/DashbordAdmin/Dashbord';
 
 
 function App() {
@@ -16,7 +16,7 @@ function App() {
       <Route path="connexion" element={<Connexion />} />
       <Route path="auth" element={<Authentification />} />
       <Route path="dashbord" element={<DashbordUtilisateur />} />
-      <Route path="dashbordadmin" element={<Dashbord />} />
+      {/* <Route path="dashbordadmin" element={<Dashbord />} /> */}
   </Routes>
   </>
   );
